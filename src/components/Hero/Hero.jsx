f2c2dc2e997d91a0edcc27fb5000c21bd6ff7e5a
Luisa_Cadena <luicadenav@gmail.com>
2025-04-15 21:51:56 -0500

    hero section mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import styles from './hero.module.css';
 import heroImage from '../../assets/hero_image.png';
+import download from '../../assets/download_icon.svg';
+import whatsapp from '../../assets/whatsapp_icon.svg';
 
 function Hero() {
   return (
@@ -8,7 +10,18 @@ function Hero() {
         <h2 className={styles.subtitle}>hello, i&apos;m</h2>
         <h1 className={styles.title}>luisa cadena</h1>
         <h2 className={styles.subtitle}>front-end developer</h2>
+        <div className={styles.buttonsContainer}>
+          <a className={styles.sharedButton} href="">
+            <img className={styles.whatsappIcon} src={whatsapp} alt="" />
+            <span>let's talk</span>
+          </a>
+          <a className={styles.sharedButton} href="">
+            <img className={styles.downloadIcon} src={download} alt="" />
+            Resume
+          </a>
+        </div>
       </div>
+
       <div className={styles.imageContainer}>
         <img src={heroImage} alt="hero Image" />
       </div>
